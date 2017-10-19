f05ab4d1c59991fc0e48358c0229d3d249b8e3f5
Brett Hargreaves <bretthargreaves@hotmail.com>
2017-10-19 16:33:15 +0100

    added generic in memory repository

--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -4,10 +4,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MyShop.Core.Models
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public string Id { get; set; }
-
         [StringLength(20)]
         [DisplayName("Product Name")]
         public string Name { get; set; }
@@ -18,10 +16,6 @@ namespace MyShop.Core.Models
         public string Category { get; set; }
         public string Image { get; set; }
 
-        public Product()
-        {
-            this.Id = Guid.NewGuid().ToString();
-        }
     }
 }
 
