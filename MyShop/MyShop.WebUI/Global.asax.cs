7b30a737d879d6cba3299f3a51a7191cd419e6e9
Brett Hargreaves <bretthargreaves@hotmail.com>
2017-10-26 14:25:17 +0100

    swapped DI implementation for Microsoft practices

--- MyShop/MyShop.WebUI/Global.asax.cs
+++ MyShop/MyShop.WebUI/Global.asax.cs
@@ -13,7 +13,6 @@ namespace MyShop.WebUI
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            UnityConfig.RegisterComponents();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
