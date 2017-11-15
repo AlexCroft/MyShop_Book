a1bf66b062c5e2418e2745d2832e854baa7c93fe
Brett Hargreaves <brett.hargreaves@capita.co.uk>
2017-11-15 21:04:38 +0000

    CustomerModel

--- MyShop/MyShop.WebUI/Controllers/AccountController.cs
+++ MyShop/MyShop.WebUI/Controllers/AccountController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using MyShop.WebUI.Models;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 
 namespace MyShop.WebUI.Controllers
 {
@@ -17,9 +19,11 @@ namespace MyShop.WebUI.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
-
-        public AccountController()
+        private IRepository<Customer> customers;
+ 
+        public AccountController(IRepository<Customer> Customers)
         {
+            this.customers = Customers;
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
@@ -155,6 +159,21 @@ namespace MyShop.WebUI.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    //register the customer account
+                    Customer customer = new Customer()
+                    {
+                        City = model.City,
+                        Email = model.Email,
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        State = model.State,
+                        Street = model.Street,
+                        ZipCode = model.ZipCode,
+                        UserId = user.Id
+                    };
+                    customers.Insert(customer);
+                    customers.Commit();
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
