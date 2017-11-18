26d07fb968caaa45f1a16b2b9658c5e08e826250
Brett Hargreaves <brett.hargreaves@capita.co.uk>
2017-11-18 20:39:58 +0000

    added order models

---
+++ MyShop/MyShop.Core/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MyShop.Core.Models
+{
+    public class Order : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string OrderStatus { get; set; }
+        public virtual ICollection<OrderItem> orderItems { get; set; }
+    }
+}
---
+++ MyShop/MyShop.Services/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Services
+{
+    public class OrderService
+    {
+        IRepository<Order> Orders;
+        IRepository<Product> Products;
+        public void CreateOrder(Order baseOrder, Basket basket) {
+            //copy the basketItems to the Order Items
+
+        }
+    }
+}
